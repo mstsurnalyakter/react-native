f2c0b04d2fe5507673af61907314da3a8e67014a
Mst Surnaly Akter <surnalyms@gmail.com>
2025-11-06 19:22:28 +0600

    feat: add @expo-google-fonts/poppins and @expo-google-fonts/inter dependencies and integrate custom font loading in Home component

--- nativeWind/app/index.tsx
+++ nativeWind/app/index.tsx
@@ -1,11 +1,24 @@
 import { Text, View } from "react-native";
+import {useFonts,Poppins_700Bold_Italic,Poppins_300Light_Italic} from "@expo-google-fonts/poppins"
 
-export default function Index() {
+
+
+export default function Home() {
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold_Italic,
+    Poppins_300Light_Italic
+  })
   return (
     <View
      className="flex-1 items-center justify-center bg-white"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+      style={{
+        fontFamily:"Poppins_700Bold_Italic",
+        fontSize:24,
+        marginBottom:12
+      }}
+      >Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
