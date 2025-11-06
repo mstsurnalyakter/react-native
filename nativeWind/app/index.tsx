87ac51c151f1902c435f84a5ad930eaabf195b8d
Mst Surnaly Akter <surnalyms@gmail.com>
2025-11-06 19:43:36 +0600

    refactor: improve code formatting and structure in Home component

--- nativeWind/app/index.tsx
+++ nativeWind/app/index.tsx
@@ -1,23 +1,35 @@
 import { Text, View } from "react-native";
-import {useFonts,Poppins_700Bold_Italic,Poppins_300Light_Italic} from "@expo-google-fonts/poppins"
+import { useFonts, Poppins_700Bold_Italic, Poppins_300Light_Italic } from "@expo-google-fonts/poppins"
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
+SplashScreen.preventAutoHideAsync();
 
 
 export default function Home() {
-  const [fontsLoaded] = useFonts({
+  const [loaded, error] = useFonts({
     Poppins_700Bold_Italic,
     Poppins_300Light_Italic
   })
+  console.log("Font loaded:", loaded);
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+
+
   return (
     <View
-     className="flex-1 items-center justify-center bg-white"
+      className="flex-1 items-center justify-center bg-white"
     >
       <Text
-      style={{
-        fontFamily:"Poppins_700Bold_Italic",
-        fontSize:24,
-        marginBottom:12
-      }}
+        style={{
+          fontFamily: "Poppins_700Bold_Italic",
+          fontSize: 24,
+          marginBottom: 12
+        }}
       >Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
